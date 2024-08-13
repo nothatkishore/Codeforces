0597822c5b0241f7d9537530d6dea25d9541098c
kishoear <kishorear.programming@gmail.com>
2024-08-14 00:17:48 +0530

    800 rated problem

--- Rating 800/A_Doremy_s_Paint_3.cpp
+++ Rating 800/A_Doremy_s_Paint_3.cpp
@@ -3,25 +3,38 @@ using namespace std;
 
 int main()
 {
-    int tests;
-    cin >> tests;
+   int tests;
+   cin >> tests;
 
-    while (tests--)
-    {
-        int n;
-        cin >> n;
-        vector<int> arr(n);
-        for (int i = 0; i < n; i++)
-            cin >> arr[i];
-        
-        int sum1 = accumulate(arr.begin(), arr.begin() + n/2 + 1, 0);
-        int sum2 = accumulate(arr.begin() + n/2 + 1, arr.end(), 0);
+   while (tests--)
+   {
+      int n;
+      cin>>n;
+      vector<int>sample(n);
+      for(int i = 0 ; i < n ; i++)
+         cin>>sample[i];
 
-        if(sum1 == sum2)
+      unordered_map<int, int>mpp;
+      for(int x : sample)
+      {
+         mpp[x]++;
+         if(mpp.size() > 2)
+            break;
+      }
+
+      if(mpp.size() == 1)
+         cout<<"Yes\n";
+      else if(mpp.size() == 2)
+      {
+         if(mpp[sample[0]] == n/2 || mpp[sample[0]] == (n + 1)/2)
             cout<<"Yes\n";
-        else
+         else
             cout<<"No\n";
-    }
+      }
+      else  
+         cout<<"No\n";
 
-    return 0;
+   }
+
+   return 0;
 }
