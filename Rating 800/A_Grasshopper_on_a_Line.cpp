e6cdc5ae00b0faf491524e171b047e65fc318689
kishoear <kishorear.programming@gmail.com>
2024-08-18 21:13:33 +0530

    rated 800

---
+++ Rating 800/A_Grasshopper_on_a_Line.cpp
@@ -0,0 +1,28 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+    int tests;
+    cin >> tests;
+
+    while (tests--)
+    {
+        int x, k;
+        cin >> x >> k;
+
+        if (x % k != 0)
+        {
+            cout<<1<<endl;
+            cout<<x<<endl;
+        }
+
+        else
+        {
+            cout<<2<<endl;
+            cout<<x - 1<<" "<<1<<endl;
+        }
+    }
+
+    return 0;
+}
