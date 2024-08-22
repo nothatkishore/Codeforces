d34c430b5d4ca231a86ce6cd829bc01ffcce666a
kishoear <kishorear.programming@gmail.com>
2024-08-22 14:35:49 +0530

    rated 800

---
+++ Rating 800/A_Two_Permutations.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Optimistic macros
+#define ll long long
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define all(x) (x).begin(), (x).end()
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+void solve()
+{
+    int n, a, b;
+    cin>>n>>a>>b;
+    if((a + b + 2) <= n)
+        cout<<"Yes";
+    else if(a == n && b == n)
+        cout<<"Yes";
+    else
+        cout<<"No";
+    cout<<endl;
+}
+
+int main()
+{
+    fast_io;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
