9be39c543ee7deb319d3d39428a9e7b278d4d74e
kishoear <kishorear.programming@gmail.com>
2024-08-26 18:57:16 +0530

    Rated 800

--- Rating 800/A_Soldier_and_Bananas.cpp
+++ Rating 800/A_Soldier_and_Bananas.cpp
@@ -11,19 +11,30 @@ using namespace std;
 #define pii pair<int, int>
 #define pll pair<ll, ll>
 #define all(x) (x).begin(), (x).end()
-#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
-const ll MOD = 1e9+7;
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+const ll MOD = 1e9 + 7;
 
 void solve()
 {
-   
+    int k, n, w;
+    cin >> k >> n >> w;
+
+    int cost = (((w * (w + 1))/2) * k) - n;
+    if(cost < 0)
+        cout<<0;
+    else
+        cout<<cost; 
 }
 
-int main() {
+int main()
+{
     fast_io;
-    int t;
-    cin >> t;
-    while (t--) {
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
         solve();
     }
 
